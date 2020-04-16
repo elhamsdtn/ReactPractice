041f6e3b947386162477b05bc709ff0e5ff81652
Ellie <elham.saadatian@gmail.com>
2020-04-16 11:26:07 -0700

    prettier

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function BookCard(props) {
-
-    return (
-      <div key={props.id} className="bookCard">
-        {props.imgUrl && (
-          <img src={props.imgUrl} alt={props.title} />
-        )}
-        <h4><a href={props.link}>{props.title}</a></h4>
-        <p>{props.description}</p>
-        <p><strong>Categories: {props.categories}</strong></p>
-      </div>
-    );
-
+  return (
+    <div key={props.id} className="bookCard">
+      {props.imgUrl && <img src={props.imgUrl} alt={props.title} />}
+      <h4>
+        <a href={props.link}>{props.title}</a>
+      </h4>
+      <p>{props.description}</p>
+      <p>
+        <strong>Categories: {props.categories}</strong>
+      </p>
+    </div>
+  );
 }
 
 export default BookCard;
